d09d6adf21b9ec16de0370a406e7f95732a4974d
firstbodi@gmail.com <macbook@ahmed.local>
2021-06-10 15:05:34 +0300

    ADD set up

--- run.py
+++ run.py
@@ -4,9 +4,9 @@ from namati.app import create_app
 
 config_name = os.getenv('APP_SETTINGS')
 
-APP = create_app(config_name)
+app = create_app(config_name)
 
 
 if __name__ == '__main__':
-    APP.secret_key = "secret_key"
-    APP.run()
+    app.secret_key = "secret_key"
+    app.run()
