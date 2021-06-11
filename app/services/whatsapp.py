eef90e8f215e10514e59e839d612853756648269
firstbodi@gmail.com <macbook@ahmed.local>
2021-06-11 11:06:57 +0300

    ADD set up

--- app/services/whatsapp.py
+++ app/services/whatsapp.py
@@ -6,6 +6,7 @@ account_sid = current_app.config['TWILIO_ACCOUNT_SID']
 auth_token = current_app.config['TWILIO_AUTH_TOKEN']
 client = Client(account_sid, auth_token)
 
+
 def send_whatsapp(sender, message, recepients):
     
     response = client.messages.create(
