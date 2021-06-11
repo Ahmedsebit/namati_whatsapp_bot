4138bf5179ae6fccea36246a20fb086edf7f398a
firstbodi@gmail.com <macbook@ahmed.local>
2021-06-11 10:53:12 +0300

    ADD set up

--- app/api/v1/messages.py
+++ app/api/v1/messages.py
@@ -1,8 +1,10 @@
 import os
 import json
 from flask import Blueprint, request, jsonify, current_app
-from app.services import respond
+from app.services import respond 
+from app.services.whatsapp import send_whatsapp
 from app.repositories import healthcheck_db
+
 
 namati_v1_api_bp = Blueprint(
     'namati_v1_api_bp', __name__,
