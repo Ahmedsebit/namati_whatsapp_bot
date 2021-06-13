177c695e358052c060567ee0acc86186562d0e78
firstbodi@gmail.com <macbook@ahmed.local>
2021-06-13 13:00:33 +0300

    ADD questions

--- app/services/chat.py
+++ app/services/chat.py
@@ -7,29 +7,31 @@ def respond(message, namati_items):
     
     if 'what' in message and 'is'  in message and 'namati' in message:
         return namati_items.get('what_is_namati')
-    if 'citizenship' in message and 'justice' in message and  'partners' in message:
+    elif 'citizenship' in message and 'justice' in message and  'partners' in message:
         return
-    if 'land' in message and 'environmental' in message and 'justicepartners' in message: 
+    elif 'land' in message and 'environmental' in message and 'justicepartners' in message: 
         return namati_items.get('land_environmental_justicepartners')
-    if 'impact' in message:
+    elif 'impact' in message:
         return namati_items.get('impact')
-    if 'injustices' in message:
+    elif 'injustices' in message:
         return namati_items.get('injustices')
-    if 'citizenship' in message and 'justice' in message:
+    elif 'citizenship' in message and 'justice' in message:
         return namati_items.get('citizenship_justice')
-    if 'land' in message and 'environmental' in message and 'justice' in message:
+    elif 'land' in message and 'environmental' in message and 'justice' in message:
         return namati_items.get('land_environmental_justice')     
-    if 'health' in message and 'justice' in message:
+    elif 'health' in message and 'justice' in message:
         return namati_items.get('health_justice')
-    if 'legal' in message and 'empowerment' in message and 'programs' in message:
+    elif 'legal' in message and 'empowerment' in message and 'programs' in message:
         return namati_items.get('legal_empowerment_programs')
-    if 'what' in message and 'we' in message and 'do' in message:
+    elif 'what' in message and 'we' in message and 'do' in message:
         return namati_items.get('what_we_do')
-    if 'grassroots' in message and 'legal' in message and 'empowerment' in message:
+    elif 'grassroots' in message and 'legal' in message and 'empowerment' in message:
         return namati_items.get('grassroots_legal_empowerment')
-    if 'recognition' in message:
+    elif 'recognition' in message:
         return namati_items.get('recognition')
-    if 'our' in message and 'mission' in message:
+    elif 'our' in message and 'mission' in message:
         return namati_items.get('our_mission')
-    if 'meaning' in message:
+    elif 'meaning' in message:
         return namati_items.get('meaning')
+    else:
+        return namati_items.get('questions_lists')
